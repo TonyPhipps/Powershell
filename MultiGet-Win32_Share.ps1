d8b990bf000d1506070030f8d70fec7590a42f75
Tony Phipps <TonyPhipps@users.noreply.github.com>
2016-10-24 11:40:14 -0400

    Update and rename Get-Win32_Win32_Share_Multi.ps1 to MultiGet-Win32_Share.ps1

--- MultiGet-Win32_Share.ps1
+++ MultiGet-Win32_Share.ps1
@@ -10,10 +10,10 @@
 #    Piped-in list of hosts/IP addresses
 #
 #.Example 
-#    get-content .\hosts.txt | Get-Win32_Share_Multi | export-csv Get-Win32_Share_Multi.csv
+#    get-content .\hosts.txt | MultiGet-Win32_Share | export-csv Get-Win32_Share_Multi.csv
 #
 #.Notes 
-# Updated: 2016-10-21
+# Updated: 2016-10-24
 # LEGAL: Copyright (C) 2016  Anthony Phipps
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -29,7 +29,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ####################################################################################
 
-Function Get-Win32_Share_Multi() {
+Function MultiGet-Win32_Share() {
 	[cmdletbinding()]
 
 
@@ -47,7 +47,7 @@ Function Get-Win32_Share_Multi() {
 			};
 		}
 		CATCH{
-			Add-Content -Path .\Get-Win32_Share_Multi_errors.txt -Value ("$thisHost");
+			Add-Content -Path .\MultiGet-Win32_Share_errors.txt -Value ("$thisHost");
 		}
 	};
 };
