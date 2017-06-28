276cec1a651b19f293e65a7a3df9eae6f0f9b8f4
Tony Phipps <TonyPhipps@users.noreply.github.com>
2017-06-28 10:48:36 -0400

    Update SCCM-GetComputer.ps1

--- SCCM-GetComputer.ps1
+++ SCCM-GetComputer.ps1
@@ -4,5 +4,7 @@ Param([parameter(Mandatory = $true)]$ComputerName,
     $SiteName="AA1",
     $SCCMServer="name.fqdn.com")
     $SCCMNameSpace="root\sms\site_$SiteName"
-    Get-WmiObject -namespace $SCCMNameSpace -computer $SCCMServer -query "select * from sms_r_system where name='$ComputerName'" | select *
+    
+    Get-WmiObject -namespace $SCCMNameSpace -computer $SCCMServer -query "select IsVirtualMachine, LastLogonTimestamp, LastLogonUserDomain, LastLogonUserDomain, LastLogonUserName, MACAddresses, OperatingSystemNameandVersion, ResourceNames from sms_r_system where name='$ComputerName'" | select IsVirtualMachine, LastLogonTimestamp, LastLogonUserDomain, LastLogonUserName, {$_.MACAddresses}, OperatingSystemNameandVersion, {$_.ResourceNames}
+    Get-WmiObject -namespace $SCCMNameSpace -computer $SCCMServer -query "select Manufacturer, Model, Domain, SystemType, UserName from SMS_G_System_Computer_System where name='$ComputerName'" | select Manufacturer, Model, Domain, SystemType, UserName
 }
