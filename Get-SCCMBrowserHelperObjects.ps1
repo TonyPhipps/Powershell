355c929e98d3ce3068a233710202a5da0c0ab11c
Tony Phipps <TonyPhipps@users.noreply.github.com>
2017-07-25 10:27:38 -0400

    Update Get-SCCMBrowserHelperObjects.ps1

--- Get-SCCMBrowserHelperObjects.ps1
+++ Get-SCCMBrowserHelperObjects.ps1
@@ -9,6 +9,14 @@ FUNCTION Get-SCCMBrowserHelperObjects {
 .Parameter Computer  
     Computer can be a single hostname, FQDN, or IP address.
 
+.Parameter CIM
+    Use Get-CIMInstance rather than Get-WMIObject. CIM cmdlets use WSMAN (WinRM)
+    to connect to remote machines, and has better standardized output (e.g. 
+    datetime format). CIM cmdlets require the querying user to be a member of 
+    Administrators or WinRMRemoteWMIUsers_ on the target system. Get-WMIObject 
+    is the default due to lower permission requirements, but can be blocked by 
+    firewalls in some environments.
+
 .Example 
     Get-SCCMBrowserHelperObjects 
     Get-SCCMBrowserHelperObjects SomeHostName.domain.com
@@ -17,7 +25,7 @@ FUNCTION Get-SCCMBrowserHelperObjects {
     Get-ADComputer -filter * | Select -ExpandProperty Name | Get-SCCMBrowserHelperObjects
 
 .Notes 
-    Updated: 2017-07-19
+    Updated: 2017-07-25
     LEGAL: Copyright (C) 2017  Anthony Phipps
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -39,7 +47,9 @@ FUNCTION Get-SCCMBrowserHelperObjects {
         [Parameter()]
         $SiteName="A1",
         [Parameter()]
-        $SCCMServer="server.domain.com"
+        $SCCMServer="server.domain.com",
+        [Parameter()]
+        [switch]$CIM
     );
 
 	BEGIN{
@@ -67,56 +77,64 @@ FUNCTION Get-SCCMBrowserHelperObjects {
             $ThisComputer = $Computer.Split(".")[0].Replace('"', '');
         };
 
-            $output = [PSCustomObject]@{
-                Name = $ThisComputer
-                ResourceNames = ""
-                Description = ""
-                FileName = ""
-                FilePropertiesHash = ""
-                FilePropertiesHashEx = ""
-                FileVersion = ""
-                Product = ""
-                ProductVersion = ""
-                Publisher = ""
-                RevisionID = ""
-                Timestamp = ""
-            };
+        $output = [PSCustomObject]@{
+            Name = $ThisComputer
+            ResourceNames = ""
+            Description = ""
+            FileName = ""
+            FilePropertiesHash = ""
+            FilePropertiesHashEx = ""
+            FileVersion = ""
+            Product = ""
+            ProductVersion = ""
+            Publisher = ""
+            RevisionID = ""
+            Timestamp = ""
+        };
 
+        if ($CIM){
+
+            $SMS_R_System = Get-CIMInstance -namespace $SCCMNameSpace -computer $SCCMServer -query "select ResourceNames, ResourceID from SMS_R_System where name='$ThisComputer'";
+            $ResourceID = $SMS_R_System.ResourceID; # Needed since -query seems to lack support for calling $SMS_R_System.ResourceID directly.
+            $SMS_G_System_BROWSER_HELPER_OBJECT = Get-CIMInstance -namespace $SCCMNameSpace -computer $SCCMServer -query "select Description, FileName, FilePropertiesHash, FilePropertiesHashEx, FileVersion, Product, ProductVersion, Publisher, RevisionID, Timestamp from SMS_G_System_BROWSER_HELPER_OBJECT where ResourceID='$ResourceID'";
+        }
+        else{
             $SMS_R_System = Get-WmiObject -namespace $SCCMNameSpace -computer $SCCMServer -query "select ResourceNames, ResourceID from SMS_R_System where name='$ThisComputer'";
             $ResourceID = $SMS_R_System.ResourceID; # Needed since -query seems to lack support for calling $SMS_R_System.ResourceID directly.
             $SMS_G_System_BROWSER_HELPER_OBJECT = Get-WmiObject -namespace $SCCMNameSpace -computer $SCCMServer -query "select Description, FileName, FilePropertiesHash, FilePropertiesHashEx, FileVersion, Product, ProductVersion, Publisher, RevisionID, Timestamp from SMS_G_System_BROWSER_HELPER_OBJECT where ResourceID='$ResourceID'";
+        };
 
-            if ($SMS_G_System_BROWSER_HELPER_OBJECT){
+        if ($SMS_G_System_BROWSER_HELPER_OBJECT){
                 
-                $SMS_G_System_BROWSER_HELPER_OBJECT | ForEach-Object {
+            $SMS_G_System_BROWSER_HELPER_OBJECT | ForEach-Object {
                 
-                    $output.ResourceNames = $SMS_R_System.ResourceNames[0];
+                $output.ResourceNames = $SMS_R_System.ResourceNames[0];
 
-                    $output.Description = $_.Description;
-                    $output.FileName = $_.FileName;
-                    $output.FilePropertiesHash = $_.FilePropertiesHash;
-                    $output.FilePropertiesHashEx = $_.FilePropertiesHashEx;
-                    $output.FileVersion = $_.FileVersion;
-                    $output.Product = $_.Product;
-                    $output.ProductVersion = $_.ProductVersion;
-                    $output.Publisher = $_.Publisher;
-                    $output.RevisionID = $_.RevisionID;
-                    $output.Timestamp = $_.Timestamp.Split(".")[0];
-
-                    return $output;
-                    $output.PsObject.Members | ForEach-Object {$output.PsObject.Members.Remove($_.Name)}; 
-                };
-            }
-            else {
+                $output.Description = $_.Description;
+                $output.FileName = $_.FileName;
+                $output.FilePropertiesHash = $_.FilePropertiesHash;
+                $output.FilePropertiesHashEx = $_.FilePropertiesHashEx;
+                $output.FileVersion = $_.FileVersion;
+                $output.Product = $_.Product;
+                $output.ProductVersion = $_.ProductVersion;
+                $output.Publisher = $_.Publisher;
+                $output.RevisionID = $_.RevisionID;
+                $output.Timestamp = $_.Timestamp;
 
                 return $output;
                 $output.PsObject.Members | ForEach-Object {$output.PsObject.Members.Remove($_.Name)}; 
             };
+        }
+        else {
 
-            $elapsed = $stopwatch.Elapsed;
-            $total = $total+1;
+            return $output;
+            $output.PsObject.Members | ForEach-Object {$output.PsObject.Members.Remove($_.Name)}; 
+        };
+
+        $elapsed = $stopwatch.Elapsed;
+        $total = $total+1;
             
-            Write-Verbose -Message "System $total `t $ThisComputer `t Time Elapsed: $elapsed";
+        Write-Verbose -Message "System $total `t $ThisComputer `t Time Elapsed: $elapsed";
 
     };
 
@@ -125,4 +143,5 @@ FUNCTION Get-SCCMBrowserHelperObjects {
         Write-Verbose "Total Systems: $total `t Total time elapsed: $elapsed";
 	};
 };
+
 
