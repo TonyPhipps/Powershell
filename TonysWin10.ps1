23f5c6c872be7bd6b8f29e3a7e91c2cc83bd4c88
Tony <anthony.james.phipps@gmail.com>
2018-03-08 23:36:26 -0500

    Overhauled

--- TonysWin10.ps1
+++ TonysWin10.ps1
@@ -1,158 +1,75 @@
 <#
+    .TODO
+    Separate user from admin
+
     .References
     https://github.com/bmrf/tron/
 #>
 
+
+
 # --- SETUP ---
 
+    function Set-RegProperty($FullPath, $PropertyType, $Value){
+        $Path = Split-Path -Path $FullPath
+        $Name = Split-Path -Path $FullPath -Leaf
 
-New-PSDrive -Name HKCR -PSProvider Registry -Root HKEY_CLASSES_ROOT | Out-Null;
+        if (!(Test-Path $Path)) {
+            
+            New-Item -ItemType Directory -Path $Path -Force -ErrorAction SilentlyContinue | Out-Null
+        }
 
-# Helper function
-    function Publish-RegPath($path) {
-        if (!(Test-Path $path)) {
-            New-Item -ItemType Directory -Force -Path $path;
-        };
-    };
+        New-ItemProperty -Path $Path -Name $Name -Value $Value -PropertyType $PropertyType -Force -ErrorAction SilentlyContinue | Out-Null
+        
+    }
 
-
-# --- SECURITY ---
-
-
-# Disable Autoplay
-    New-ItemProperty -Path "HKCU:Software\Microsoft\Windows\CurrentVersion\Explorer\AutoplayHandlers" -Name "DisableAutoplay" -Value "1" -PropertyType "DWORD" -Force;
-
-# Adjust Screen Saver Password Protected Grace Period (10sec)
-    New-ItemProperty -Path "HKLM:Software\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name "ScreenSaverGracePeriod" -Value "10" -PropertyType "DWORD" -Force;
-
-
-# --- NUISANCE ---
-
-
-# Disable Tips, Tricks, and Suggestions Notifications
-    New-ItemProperty -Path "HKCU:Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" -Name "SoftLandingEnabled" -Value "0" -PropertyType "DWORD" -Force;
-
-# Disable Cortana Taskbar Tidbits
-    Publish-RegPath "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search";
-    New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search" -Name "AllowCortana" -Value "0" -PropertyType "DWORD" -Force;
-
-# Disable Fast Startup
-    New-ItemProperty -Path "HKLM:SYSTEM\CurrentControlSet\Control\Session Manager\Power" -Name "HiberbootEnabled" -Value "0" -PropertyType "DWORD" -Force;
-
-
-
-# --- PREFERENCE ---
-
-
-# Set taskbar tray icons to always show (0) or hide (1)
-    New-ItemProperty -Path "HKCU:Software\Microsoft\Windows\CurrentVersion\Explorer" -Name "EnableAutoTray" -Value "1" -PropertyType "DWORD" -Force;
-
-# Set taskbar icons icons to small (1) or large (0)
-    New-ItemProperty -Path "HKCU:Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "TaskbarSmallIcons" -Value "1" -PropertyType "DWORD" -Force;
-
-# Apply dark app theme
-    New-ItemProperty -Path "HKCU:Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "AppsUseLightTheme" -Value "0" -PropertyType "DWORD" -Force;
-
-# Remove apps
-    $packages = @(
-    "*Advertising*"
-    "*WindowsMaps*"
-    "*People*"
-    "*bing*"
-    "*zune*"
-    "*xbox*"
-    "*twitter*"
-    "Microsoft.CommsPhone"
-    "Microsoft.ConnectivityStore"
-    "Microsoft.Getstarted"
-    "Microsoft.Messaging"
-    "Microsoft.OneConnect"
-    );
     
-    foreach ($package in $packages) {
-        Get-AppxPackage $package | Remove-AppxPackage -ErrorAction SilentlyContinue;
-    };
+# --- USER PREFERENCE ---
 
-# Remove OneDrive completely
-    $OneDrivex86 = "$env:SystemRoot\System32\OneDriveSetup.exe";
-    $OneDrivex64 = "$env:SystemRoot\SysWOW64\OneDriveSetup.exe";
- 
-    Get-Process *OneDrive* | Stop-Process -Force | Out-Null;
-    Start-Sleep 3;
- 
-    if (Test-Path $OneDrivex86)
-    {
-        & $OneDrivex86 "/uninstall" | Out-Null;
-    };
- 
-    if (Test-Path $OneDrivex64)
-    {
-        & $OneDrivex64 "/uninstall" | Out-Null;
-    };
-    Start-Sleep 15; # Uninstallation needs time to let go off the files
- 
-    # Explorer.exe gets in our way by locking the files for some reason
-    taskkill /F /IM explorer.exe | Out-Null;
-    
-    if (Test-Path "$env:USERPROFILE\OneDrive") { 
-        Remove-Item "$env:USERPROFILE\OneDrive" -Recurse -Force | Out-Null 
-    };
-    
-    if (Test-Path "C:\OneDriveTemp") { 
-        Remove-Item "C:\OneDriveTemp" -Recurse -Force | Out-Null 
-    };
-    
-    if (Test-Path "$env:LOCALAPPDATA\Microsoft\OneDrive") { 
-        Remove-Item "$env:LOCALAPPDATA\Microsoft\OneDrive" -Recurse -Force | Out-Null 
-    };
-    
-    if (Test-Path "$env:PROGRAMDATA\Microsoft OneDrive") { 
-        Remove-Item "$env:PROGRAMDATA\Microsoft OneDrive" -Recurse -Force | Out-Null 
-    };
-    
-    Start-Process explorer.exe;
-
-    # Remove OneDrive from the Explorer Side Panel
-    if (Test-Path "HKCR:\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}") { 
-        Remove-Item -Force -Path "HKCR:\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" -Recurse | Out-Null 
-    };
-    
-    if (Test-Path "HKCR:\Wow6432Node\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}") { 
-        Remove-Item -Force -Path "HKCR:\Wow6432Node\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" -Recurse | Out-Null
-    };
-
-# Folder view options
     # Show Hidden Files
-    Set-ItemProperty "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" "Hidden" 1;
+    Set-RegProperty "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\Hidden" "DWORD" 1
 
     # Show file extensions
-    Set-ItemProperty "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" "HideFileExt" 0;
+    Set-RegProperty "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\HideFileExt" "DWORD" 0
 
     # Show drives with no media
-    Set-ItemProperty "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" "HideDrivesWithNoMedia" 0;
- 
-# Remove 'Share with' from context menu
-    Remove-Item -Path "HKCR:\Directory\Background\shellex\ContextMenuHandlers\Sharing" -Force -Recurse | Out-Null;
-    Remove-Item -Path "HKCR:\Directory\shellex\ContextMenuHandlers\Sharing" -Force -Recurse | Out-Null;
-    Remove-Item -Path "HKCR:\Directory\shellex\CopyHookHandlers\Sharing" -Force -Recurse | Out-Null;
-    Remove-Item -Path "HKCR:\Directory\shellex\PropertySheetHandlers\Sharing" -Force -Recurse | Out-Null;
-    Remove-Item -Path "HKCR:\Drive\shellex\ContextMenuHandlers\Sharing" -Force -Recurse | Out-Null;
-    Remove-Item -Path "HKCR:\Drive\shellex\PropertySheetHandlers\Sharing" -Force -Recurse | Out-Null;
-    Remove-Item -Path "HKCR:\LibraryFolder\background\shellex\ContextMenuHandlers\Sharing" -Force -Recurse | Out-Null;
-    Remove-Item -Path "HKCR:\UserLibraryFolder\shellex\ContextMenuHandlers\Sharing" -Force -Recurse | Out-Null;
-    New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name SharingWizardOn -PropertyType DWORD -Value 0 -Force | Out-Null;
+    Set-RegProperty "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\HideDrivesWithNoMedia" "DWORD" 0
 
-# Remove 'Include in library' from context menu
-    Remove-Item "HKLM:\SOFTWARE\Classes\Folder\ShellEx\ContextMenuHandlers\Library Location" -Force -Recurse | Out-Null;
+    # Set taskbar tray icons to always show (0) or hide (1)
+    Set-RegProperty "HKCU:Software\Microsoft\Windows\CurrentVersion\Explorer\EnableAutoTray" "DWORD" 1
 
-# Remove 'Send to' from context menu
-    Remove-Item -Path "HKCR:\AllFilesystemObjects\shellex\ContextMenuHandlers\SendTo" -Force -Recurse | Out-Null;
+    # Set taskbar icons icons to small (1) or large (0)
+    Set-RegProperty "HKCU:Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced\TaskbarSmallIcons" "DWORD" 1
 
-# Disable Services
+    # Apply dark app theme
+    Set-RegProperty "HKCU:Software\Microsoft\Windows\CurrentVersion\Themes\Personalize\AppsUseLightTheme" "DWORD" 0
+    
+    # Disable Tips, Tricks, and Suggestions Notifications
+    Set-RegProperty "HKCU:Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager\SoftLandingEnabled" "DWORD" 0
+
+    # Disable Autoplay 
+    Set-RegProperty "HKCU:Software\Microsoft\Windows\CurrentVersion\Explorer\AutoplayHandlers\DisableAutoplay" "DWORD" 1
+
+
+
+# --- SYSTEM PREFERENCE ---
+
+    New-PSDrive -Name HKCR -PSProvider Registry -Root HKEY_CLASSES_ROOT -ErrorAction SilentlyContinue | Out-Null
+
+    # Adjust Screen Saver Password Protected Grace Period (10sec)
+    Set-RegProperty "HKLM:Software\Microsoft\Windows NT\CurrentVersion\Winlogon\ScreenSaverGracePeriod" "DWORD" 10
+
+    # Disable Cortana Taskbar Tidbits
+    Set-RegProperty "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search\AllowCortana" "DWORD" 0
+
+    # Disable Fast Startup
+    Set-RegProperty "HKLM:SYSTEM\CurrentControlSet\Control\Session Manager\Power\HiberbootEnabled" "DWORD" 0
+
+    # Disable Services
     $services = @(
         "diagnosticshub.standardcollector.service" # Microsoft (R) Diagnostics Hub Standard Collector Service
         "DiagTrack"                                # Diagnostics Tracking Service
-        "dmwappushservice"                         # WAP Push Message Routing Service (see known issues)
+        "dmwappushservice"                         # WAP Push Message Routing Service
         "HomeGroupListener"                        # HomeGroup Listener
         "HomeGroupProvider"                        # HomeGroup Provider
         "lfsvc"                                    # Geolocation Service
@@ -167,13 +84,73 @@ New-PSDrive -Name HKCR -PSProvider Registry -Root HKEY_CLASSES_ROOT | Out-Null;
         "XblAuthManager"                           # Xbox Live Auth Manager
         "XblGameSave"                              # Xbox Live Game Save Service
         "XboxNetApiSvc"                            # Xbox Live Networking Service
-    );
+    )
 
     foreach ($service in $services) {
-        Write-Output "Trying to disable $service";
-        Get-Service -Name $service | Set-Service -StartupType Disabled;
-    };
+        Write-Output "Trying to disable $service"
+        Get-Service -Name $service | Set-Service -StartupType Disabled
+    }
+
+
+
+# --- SOFTWARE/FEATURE REMOVAL ---  
+
+    # Remove apps
+    $packages = @(
+        "*Advertising*"
+        "*WindowsMaps*"
+        "*People*"
+        "*bing*"
+        "*zune*"
+        "*xbox*"
+        "*twitter*"
+        "Microsoft.CommsPhone"
+        "Microsoft.ConnectivityStore"
+        "Microsoft.Getstarted"
+        "Microsoft.Messaging"
+        "Microsoft.OneConnect"
+    )
+    
+    foreach ($package in $packages) {
+        Get-AppxPackage $package | Remove-AppxPackage -ErrorAction SilentlyContinue
+    }
+
+    # Remove OneDrive completely
+    $OneDrivex86 = "$env:SystemRoot\System32\OneDriveSetup.exe"
+    $OneDrivex64 = "$env:SystemRoot\SysWOW64\OneDriveSetup.exe"
+ 
+    Get-Process *OneDrive* | Stop-Process -Force | Out-Null
+    Start-Sleep 3
+ 
+    if (Test-Path $OneDrivex86)
+    {
+        & $OneDrivex86 "/uninstall" | Out-Null
+    }
+ 
+    if (Test-Path $OneDrivex64)
+    {
+        & $OneDrivex64 "/uninstall" | Out-Null
+    }
+    Start-Sleep 15 # Uninstallation needs time to let go off the files
+ 
+    taskkill /F /IM explorer.exe | Out-Null     # Explorer.exe gets in our way by locking the files for some reason
+
+    Remove-Item "$env:USERPROFILE\OneDrive" -Force -Recurse -ErrorAction SilentlyContinue | Out-Null
+    Remove-Item "C:\OneDriveTemp" -Force -Recurse -ErrorAction SilentlyContinue | Out-Null
+    Remove-Item "$env:LOCALAPPDATA\Microsoft\OneDrive" -Force -Recurse -ErrorAction SilentlyContinue | Out-Null
+    Remove-Item "$env:PROGRAMDATA\Microsoft OneDrive" -Force -Recurse -ErrorAction SilentlyContinue | Out-Null
+
+    # Remove OneDrive from the Explorer Side Panel
+    Remove-Item -Path "HKCR:\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" -Force -Recurse -ErrorAction SilentlyContinue | Out-Null
+    Remove-Item -Path "HKCR:\Wow6432Node\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" -Force -Recurse -ErrorAction SilentlyContinue | Out-Null 
+
+    Start-Process explorer.exe
+
+
+
+# --- CLEANUP ---
 
 # Restart explorer to cause some settings to take affect
-    Stop-Process -ProcessName explorer;
+    Stop-Process -ProcessName explorer
+
 
