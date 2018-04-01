eb9e698882a05ad6e1ac8667971230a842d22c0a
unknown <anthony.james.phipps@gmail.com>
2018-04-01 14:13:11 -0400

    expanded

--- deduplicate_files.ps1
+++ deduplicate_files.ps1
@@ -1,3 +1,3 @@
-﻿@(ls *.*).Count
-ls *.* | get-filehash | group -property hash | where { $_.count -gt 1 } | % { $_.group | select -skip 1 } | del
-@(ls *.*).Count
+﻿@(Get-ChildItem *.*).Count
+Get-ChildItem *.* | get-filehash | Group-Object -property hash | Where-Object { $_.count -gt 1 } | ForEach-Object { $_.group | Select-Object -skip 1 } | Remove-Item
+@(Get-ChildItem *.*).Count
