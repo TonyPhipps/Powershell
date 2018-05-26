f7563aa9d4f7608ef549750e96d3c376549e1ab1
unknown <anthony.james.phipps@gmail.com>
2018-05-25 21:50:40 -0400

    update

--- Docker-ELK.ps1
+++ Docker-ELK.ps1
@@ -5,7 +5,7 @@ docker run -p 5601:5601 -p 9200:9200 -p 5044:5044 -it --name elk sebp/elk
 # https://hub.docker.com/r/sebp/elk/
 
 # INITIALIZE KIBANA WITH DUMMY LOG
-# (do this part with regular powershell, not ISE)
+# (do this part with regular powershell, not ISE... which freezes)
 # docker ps
 # docker exec -it <container-name> /bin/bash
 # /opt/logstash/bin/logstash --path.data /tmp/logstash/data \
