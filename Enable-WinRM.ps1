9bdc04f14313e02cc2063f22111c557c843cd050
unknown <anthony.james.phipps@gmail.com>
2018-06-12 21:26:11 -0400

    Added options

--- Enable-WinRM.ps1
+++ Enable-WinRM.ps1
@@ -1,6 +1,16 @@
-#Start the WinRM service. 
+# Option 1
+# Shortest route
+winrm quickconfig -y
+
+# Option 2
+# Same as above, but more "manual"
+# Start the WinRM service. 
 (get-service winrm).start()
-#Set the WinRM service type to delayed auto start.
+# Set the WinRM service type to delayed auto start.
 set-service winrm -startuptype "Automatic"
-#Configure LocalAccountTokenFilterPolicy to grant administrative rights remotely to local users.
+# Configure LocalAccountTokenFilterPolicy to grant administrative rights remotely to local users.
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name LocalAccountTokenFilterPolicy -Value 1
+
+# Option 3
+# Run winrm quickconfig on a remote system
+PsExec.exe -s \\SomeComputer -accepteula powershell -ExecutionPolicy ByPass -nologo -command "& winrm quickconfig -y"
