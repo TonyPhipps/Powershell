6302ce68762299b635339e3aabae5de1ea2e1431
unknown <anthony.james.phipps@gmail.com>
2018-07-20 22:00:49 -0400

    Replaced with Get-DuplicateFiles

--- deduplicate_files.ps1
+++
@@ -1,3 +0,0 @@
-﻿@(Get-ChildItem *.*).Count
-Get-ChildItem *.* | get-filehash | Group-Object -property hash | Where-Object { $_.count -gt 1 } | ForEach-Object { $_.group | Select-Object -skip 1 } | Remove-Item
-@(Get-ChildItem *.*).Count
