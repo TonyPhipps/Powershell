c5e9e42a43019a9877417a28b42f3545942e1b0c
Tony Phipps <anthony.james.phipps@gmail.com>
2018-08-05 20:43:52 -0400

    update

--- Create-ScheduledTask.ps1
+++ Create-ScheduledTask.ps1
@@ -2,7 +2,11 @@
 
 $Action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument 'C:\PowershellScript.ps1'
 
+# Repeat Daily
 $Trigger = New-ScheduledTaskTrigger -Daily -At 10am
+
+# Repeat Every Hour
+$Trigger2 = New-ScheduledTaskTrigger -Once -At (Get-Date) -RepetitionDuration (New-TimeSpan -Days (365 * 20)) -RepetitionInterval  (New-TimeSpan -Minutes 60)
 
 Register-ScheduledTask -Action $Action -Trigger $Trigger -TaskName "Powershell Script" -Description "Why did I do this?"
 
