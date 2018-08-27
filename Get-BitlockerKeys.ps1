65f826f3c5222c4ed7126be5bdfc89449d8d5858
Tony Phipps <TonyPhipps@users.noreply.github.com>
2018-08-27 10:28:27 -0400

    Update Get-BitlockerKeys.ps1

--- Get-BitlockerKeys.ps1
+++ Get-BitlockerKeys.ps1
@@ -1,4 +1,4 @@
 ForEach ($Volume in Get-BitLockerVolume) {
-    $Volume | Add-Member -MemberType NoteProperty -Name Key -Value ((Get-BitLockerVolume).KeyProtector.RecoveryPassword[1])
+    $Volume | Add-Member -MemberType NoteProperty -Name Key -Value (($Volume).KeyProtector.RecoveryPassword[1])
     $Volume | Select-Object *
 }
