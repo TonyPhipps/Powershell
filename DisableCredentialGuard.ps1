e061eac0f3cb30cb2feb52487e8cb22f397bfe30
Tony Phipps <TonyPhipps@users.noreply.github.com>
2018-09-01 18:35:46 -0400

    Create DisableCredentialGuard.ps1

---
+++ DisableCredentialGuard.ps1
@@ -0,0 +1,10 @@
+# https://kb.vmware.com/s/article/2146361
+
+mountvol X: /s
+copy-item %WINDIR%\System32\SecConfig.efi X:\EFI\Microsoft\Boot\SecConfig.efi -force
+bcdedit /create {0cb3b571-2f2e-4343-a879-d86a476d7215} /d "DebugTool" /application osloader
+bcdedit /set {0cb3b571-2f2e-4343-a879-d86a476d7215} path "\EFI\Microsoft\Boot\SecConfig.efi"
+bcdedit /set {bootmgr} bootsequence {0cb3b571-2f2e-4343-a879-d86a476d7215}
+bcdedit /set {0cb3b571-2f2e-4343-a879-d86a476d7215} loadoptions DISABLE-LSA-ISO,DISABLE-VBS
+bcdedit /set {0cb3b571-2f2e-4343-a879-d86a476d7215} device partition=X:
+mountvol X: /d
