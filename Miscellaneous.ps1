24f26caa53c11764bf55c65ae1a44a6db435ac58
Tony Phipps <anthony.james.phipps@gmail.com>
2019-03-05 21:22:51 -0500

    Update Miscellaneous.ps1

--- Miscellaneous.ps1
+++ Miscellaneous.ps1
@@ -1,7 +1,8 @@
 # Force Get-Childitem to NOT pull Windows directory or Program Files directories.
-
 Get-ChildItem c:\ -Depth 0 -Directory | Where-Object {$_.Name -notmatch "windows|Program Files|Program Files \(x86\)"} | Get-Childitem -Recurse
 
 # Get Windows 7, 10 Product Key from current system
-
 (Get-WmiObject -Query 'select * from SoftwareLicensingService').OA3xOriginalProductKey
+
+# Clear all event logs
+wevtutil el | Foreach-Object {wevtutil cl "$_"}
