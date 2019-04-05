1aaf4553eacd9252815464d0e502b0309b8828f5
Tony Phipps <anthony.james.phipps@gmail.com>
2019-04-04 21:13:15 -0400

    updates

--- Miscellaneous.ps1
+++ Miscellaneous.ps1
@@ -6,3 +6,13 @@ Get-ChildItem c:\ -Depth 0 -Directory | Where-Object {$_.Name -notmatch "windows
 
 # Clear all event logs
 wevtutil el | Foreach-Object {wevtutil cl "$_"}
+
+# List mapped drives
+get-wmiobject -class 'Win32_LogicalDisk' -Filter 'drivetype=4'
+
+
+# Query a for a registry value's data at the given key
+$key = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\EventLog\EventForwarding\SubscriptionManager"
+$value = "1"
+$data = (Get-ItemProperty -path $key).$value
+$data
