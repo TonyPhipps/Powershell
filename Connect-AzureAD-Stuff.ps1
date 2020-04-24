03442c778357a152163c1b41dcc780efeb5c2ae0
Tony Phipps <anthony.james.phipps@gmail.com>
2020-04-23 18:40:22 -0600

    Update Connect-AzureAD-Stuff.ps1

--- Connect-AzureAD-Stuff.ps1
+++ Connect-AzureAD-Stuff.ps1
@@ -1,2 +1,2 @@
 # Get AppID DisplayName for lookup table
-$Pricipals | Select-Object AppId, Displayname | Sort-Object DisplayName | export-csv -NoTypeInformation principal-appid.csv
+Get-AzureADServicePrincipal -All:$True | Select-Object AppId, Displayname | Sort-Object DisplayName | export-csv -NoTypeInformation principal-appid.csv
