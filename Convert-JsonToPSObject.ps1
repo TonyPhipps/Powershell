1b9c8f90f4dbae4c1ca94f3c03ef871bddc34c5f
Tony Phipps <anthony.james.phipps@gmail.com>
2020-11-14 17:47:36 -0700

    Delete Convert-JsonToPSObject.ps1

--- Convert-JsonToPSObject.ps1
+++
@@ -1,6 +0,0 @@
-$file = "file.json"
-$json = Get-Content $file | ConvertFrom-Json
-$json
-# Note that some json nests the records deeper into the array. For example:
-$records = $json._embedded.records
-$records
