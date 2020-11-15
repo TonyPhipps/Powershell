c9f1451d70620b9c6066542cb04b1970ed1f1b91
Tony Phipps <anthony.james.phipps@gmail.com>
2020-11-14 17:53:04 -0700

    Delete Get-BitlockerKeys.ps1

--- Get-BitlockerKeys.ps1
+++
@@ -1,4 +0,0 @@
-ForEach ($Volume in Get-BitLockerVolume) {
-    $Volume | Add-Member -MemberType NoteProperty -Name Key -Value (($Volume).KeyProtector.RecoveryPassword[1])
-    $Volume | Select-Object *
-}
