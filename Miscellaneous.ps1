da589871ade68395dfc810deff0afbb7521a56c8
Tony Phipps <anthony.james.phipps@gmail.com>
2020-11-14 17:46:06 -0700

    Update Miscellaneous.ps1

--- Miscellaneous.ps1
+++ Miscellaneous.ps1
@@ -28,3 +28,22 @@ Get-Partition -DiskNumber 1
 Remove-Partition -DiskNumber 1 -PartitionNumber 1
 New-Partition -DiskNumber 1 -UseMaximumSize -AssignDriveLetter
 Format-Volume -DriveLetter F -FileSystem NTFS -NewFileSystemLabel "Label"
+
+
+# Enable WinRM Option 1
+winrm quickconfig -y
+
+# Enable WinRM Option 2
+Enable-PSRemoting –Force
+
+# Enable WinRM Option 3
+## Start the WinRM service. 
+(get-service winrm).start()
+## Set the WinRM service type to delayed auto start.
+set-service winrm -startuptype "Automatic"
+## Configure LocalAccountTokenFilterPolicy to grant administrative rights remotely to local users.
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name LocalAccountTokenFilterPolicy -Value 1
+
+# Enable WinRM Option 4
+## Run winrm quickconfig on a remote system
+PsExec.exe -s \\SomeComputer -accepteula powershell -ExecutionPolicy ByPass -nologo -command "& winrm quickconfig -y"
