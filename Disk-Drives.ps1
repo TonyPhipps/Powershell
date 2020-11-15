836b778d9ff51fd01326c0e8ece9444c91f108c5
Tony Phipps <anthony.james.phipps@gmail.com>
2020-11-14 17:44:41 -0700

    Delete Disk-Drives.ps1

--- Disk-Drives.ps1
+++
@@ -1,5 +0,0 @@
-Get-Disk
-Get-Partition -DiskNumber 1
-Remove-Partition -DiskNumber 1 -PartitionNumber 1
-New-Partition -DiskNumber 1 -UseMaximumSize -AssignDriveLetter
-Format-Volume -DriveLetter F -FileSystem NTFS -NewFileSystemLabel "Label"
