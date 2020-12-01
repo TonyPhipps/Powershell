168a988fddbae9bc7be190eeaef9e9c41630338b
Tony Phipps <anthony.james.phipps@gmail.com>
2020-11-30 21:31:48 -0700

    Update Set-NameToDateTaken.ps1

--- Set-NameToDateTaken.ps1
+++ Set-NameToDateTaken.ps1
@@ -1,6 +1,6 @@
 # Renames .jpg files to the date they were taken, according to EXIF metadata
 
-Get-ChildItem C:\temp\test\*.jpg | ForEach-Object {
+Get-ChildItem *.jpg | ForEach-Object {
 
     $FileName = $_.FullName
 
@@ -8,22 +8,19 @@ Get-ChildItem C:\temp\test\*.jpg | ForEach-Object {
     
     $pic = New-Object System.Drawing.Bitmap($FileName)
 
-    try {$DateTakenRaw = $pic.GetPropertyItem(36867).Value} catch {}
-
-	if ($DateTakenRaw) {
-
+    try {
+        $DateTakenRaw = $pic.GetPropertyItem(36867).Value
         $DateTakenString = [System.Text.Encoding]::ASCII.GetString($DateTakenRaw)
         $DateTakenParsed = [datetime]::ParseExact($DateTakenString, "yyyy:MM:dd HH:mm:ss`0", $Null)
         $pic.Dispose()
-
+        Write-Verbose $DateTakenParsed
     }
-    else {
-        
-        # Fall back to LastWriteTime
-		$DateTakenParsed = $_.LastWriteTime
-	}
+    catch {}
+    finally {
+        if ($DateTakenRaw) {
 
-	$NewName = ("{0:yyyy-MM-dd_HH-mm-ss}_{1}{2}" -f $DateTakenParsed, $_.Length, $_.Extension)
-
-	rename-item $FileName $NewName.ToLower() -Force
+            $NewName = ("{0:yyyy-MM-dd_HH-mm-ss}_{1}{2}" -f $DateTakenParsed, $_.Length, $_.Extension)
+            rename-item $FileName $NewName.ToLower() -Force
+        }
+    }
 }
