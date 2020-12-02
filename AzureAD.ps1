42f771dde134014c1cf9ab2e527e3eeb8b4c058c
Tony Phipps <anthony.james.phipps@gmail.com>
2020-12-02 08:58:46 -0700

    Update and rename Get-AzureADSignInLogs.ps1 to AzureAD.ps1

--- AzureAD.ps1
+++ AzureAD.ps1
@@ -7,9 +7,22 @@ Connect-AzureAD -Credential $UserCredential
 
 $UPN = "tony@sample.com"
 
+
 # Get All Logs
 Get-AzureADAuditSignInLogs -Filter "userPrincipalName eq '$UPN'"
+
+
+# Save Logs to JSON File
+$Logs = Get-AzureADAuditSignInLogs -Filter "userPrincipalName eq '$UPN'"
+
+foreach ($log in $Logs){
+  $log | ConvertTo-Json -Compress | Out-File AzureADAuditSignInLogs.json -Append
+}
+
 
 # Get Last Login
 Get-AzureAdAuditSigninLogs -top 1 -filter "userprincipalname eq '$UPN'" | select CreatedDateTime
 
+
+# Create a lookup table for ResourceAppId GUID's
+Get-AzureADServicePrincipal -All:$True | Select-Object AppId, Displayname | Sort-Object DisplayName | export-csv -NoTypeInformation principal-appid.csv
