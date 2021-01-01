03c19113c2c38494e76d6b49d61c8f8a7981dd5d
Tony Phipps <anthony.james.phipps@gmail.com>
2021-01-01 11:41:21 -0700

    Update ExchangePowerShellScripts.ps1

--- ExchangePowerShellScripts.ps1
+++ ExchangePowerShellScripts.ps1
@@ -1,2 +1,5 @@
 # Get Litigation Hold Status
 Get-Mailbox user@tdomain.com | Format-List *lit*
+
+# Get inbox rules
+Get-InboxRule -Mailbox "[email@address.com]" | Select Name, Description, Enabled, Priority, ForwardTo, ForwardAsAttachmentTo, RedirectTo, DeleteMessage 
