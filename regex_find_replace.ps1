f9d1e5c4eedce9ed350476bb149433b91ec0dad9
Tony Phipps <anthony.james.phipps@gmail.com>
2021-01-05 09:02:09 -0700

    Update regex_find_replace.ps1

--- regex_find_replace.ps1
+++ regex_find_replace.ps1
@@ -1,14 +1,33 @@
+# Searches a directory for matching files then executes regex-based find and replace
+
 $path="c:\logs"
 
 Get-ChildItem $path -Filter *.log -Recurse | 
     ForEach-Object {
         $matches = Select-String -Path $_.FullName -Pattern "<stuff>(.*)</stuff>" -Encoding unicode
         $matches = $matches.Matches
-                $values = foreach ($match in $matches){
-					$value = $match.groups[1].value
-					$value = $value.replace("&lt;","<")
-					$value = $value.replace("&gt;",">")
-					$value  
+            $values = foreach ($match in $matches){
+                $value = $match.groups[1].value
+                $value = $value.replace("&lt;","<")
+                $value = $value.replace("&gt;",">")
+                $value  
             }
         
         $values | Out-File -FilePath ($path+'\processed\'+$_.BaseName + '.log')}
+
+
+# Opens a file and executes regex-based find and replace
+$log = "C:\test.txt"
+
+$matches = Select-String -path $log -Pattern "<stuff>(.*)</stuff>" -Encoding unicode
+
+$matches = $matches.matches
+
+$values = foreach ($match in $matches){
+    $value = $match.groups[1].value
+    $value = $value.replace("&lt;","<")
+    $value = $value.replace("&gt;",">")
+    $value
+}
+
+$values | Out-File -FilePath processed.txt
