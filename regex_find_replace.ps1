bdf56d097230400209cad7e6820e86fca628b687
Tony Phipps <anthony.james.phipps@gmail.com>
2021-01-05 12:32:41 -0700

    Update regex_find_replace.ps1

--- regex_find_replace.ps1
+++ regex_find_replace.ps1
@@ -1,19 +1,29 @@
 # Searches a directory for matching files then executes regex-based find and replace
 
-$path="c:\logs"
+$path="C:\logs\"
+mkdir "$path\processed"
 
-Get-ChildItem $path -Filter *.log -Recurse | 
+Get-ChildItem $path -Filter *.bak -Recurse | 
     ForEach-Object {
-        $matches = Select-String -Path $_.FullName -Pattern "<stuff>(.*)</stuff>" -Encoding unicode
+        $content = Get-Content $_.FullName -Encoding Unicode
+        $content = $content -replace("\x00","`n")
+        $content = $content.Split([Environment]::NewLine)
+        $content = $content -replace("&lt;","<")
+        $content = $content -replace("&gt;",">")
+        $content = $content -replace("&quot;",'"')
+
+        $matches = $content | Select-String -Pattern "<rawsyslogmsg>(.*?)</rawsyslogmsg>"
         $matches = $matches.Matches
-            $values = foreach ($match in $matches){
-                $value = $match.groups[1].value
-                $value = $value.replace("&lt;","<")
-                $value = $value.replace("&gt;",">")
-                $value  
-            }
-        
-        $values | Out-File -FilePath ($path+'\processed\'+$_.BaseName + '.log')}
+        $values = foreach ($match in $matches){
+            $value = $match.groups[1].value
+            $value = $value.replace("&lt;","<")
+            $value = $value.replace("&gt;",">")
+            $value
+        }
+
+        $values | Out-File -FilePath ($path+'\processed\'+$_.BaseName + '.log')
+    }
+
 
 
 # Opens a file and executes regex-based find and replace
