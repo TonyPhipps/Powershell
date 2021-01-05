ee685bde7b36792fc995b323b2d65ee01280855c
Tony Phipps <anthony.james.phipps@gmail.com>
2021-01-05 08:51:41 -0700

    Update regex_find_replace.ps1

--- regex_find_replace.ps1
+++ regex_find_replace.ps1
@@ -1,14 +1,14 @@
-$log = "C:\test.txt"
+$path="c:\logs"
 
-$matches = Select-String -path $log -Pattern "<junk>(.*)</junk>" -Encoding unicode
-
-$matches = $matches.matches
-
-$values = foreach ($match in $matches){
-    $value = $match.groups[1].value
-    $value = $value.replace("&lt;","<")
-    $value = $value.replace("&gt;",">")
-    $value
-}
-
-$values | Out-File -FilePath processed.txt
+Get-ChildItem $path -Filter *.log -Recurse | 
+    ForEach-Object {
+        $matches = Select-String -Path $_.FullName -Pattern "<stuff>(.*)</stuff>" -Encoding unicode
+        $matches = $matches.Matches
+                $values = foreach ($match in $matches){
+					$value = $match.groups[1].value
+					$value = $value.replace("&lt;","<")
+					$value = $value.replace("&gt;",">")
+					$value  
+            }
+        
+        $values | Out-File -FilePath ($path+'\processed\'+$_.BaseName + '.log')}
