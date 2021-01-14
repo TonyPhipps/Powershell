ff2b84572b03fda808b1c81cf421f89e3049820a
Tony Phipps <anthony.james.phipps@gmail.com>
2021-01-14 14:02:00 -0700

    Delete Microsoft365.ps1

--- Microsoft365.ps1
+++
@@ -1,6 +0,0 @@
-# References
-# - https://docs.microsoft.com/en-us/microsoft-365/compliance/mailitemsaccessed-forensics-investigations?view=o365-worldwide
-
-#Run the following command to sign-out the user of all active sessions.
-Connect-AzureAD
-Get-AzureADUser -SearchString user@contoso.com | Revoke-AzureADUserAllRefreshToken 
