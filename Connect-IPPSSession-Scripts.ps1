b60b9ecf6dd16eb029f0290ae0fa441717dcee10
Tony Phipps <anthony.james.phipps@gmail.com>
2021-02-05 11:22:29 -0700

    Update Connect-IPPSSession-Scripts.ps1

--- Connect-IPPSSession-Scripts.ps1
+++ Connect-IPPSSession-Scripts.ps1
@@ -1,3 +1,7 @@
+# Configure
+# https://docs.microsoft.com/en-us/powershell/exchange/connect-to-scc-powershell?view=exchange-ps
+Import-Module ExchangeOnlineManagement
+
 # Connect
 $UserCredential = Get-Credential
 Connect-IPPSSession -Credential $UserCredential
