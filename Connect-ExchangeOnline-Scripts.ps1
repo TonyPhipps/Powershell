a516c98b992cc3923d4a6e633563f7a659c708d4
Tony Phipps <anthony.james.phipps@gmail.com>
2021-02-05 11:26:32 -0700

    Update Connect-ExchangeOnline-Scripts.ps1

--- Connect-ExchangeOnline-Scripts.ps1
+++ Connect-ExchangeOnline-Scripts.ps1
@@ -17,3 +17,13 @@ Set-Mailbox kimakers@contoso.com -RecipientLimits 20
 # Update the default  Recipient Limits for new mailboxes created in the future (all plans, tenant-level)
 Get-MailboxPlan | Set-MailboxPlan -RecipientLimits 50
 Set-TransportConfig -MaxRecipientEnvelopeLimit 50
+
+# Get UnifiedAuditLog, last 365 days
+$UserCredential = Get-Credential
+Connect-ExchangeOnline -Credential $UserCredential -ShowProgress $true
+
+$mailbox="tony@company.com"
+$StartDate = (Get-Date).AddDays(-365)
+$EndDate = get-date
+$UnifiedAuditLog = Search-UnifiedAuditLog -UserIds $mailbox -StartDate $StartDate -EndDate $EndDate -SessionCommand ReturnLargeSet -ResultSize 5000
+$UnifiedAuditLog | Select-Object CreationDate, UserIDs, Operations, AuditData | Export-Csv ($mailbox + "_UnifiedAuditLog.csv") -NoTypeInformation
