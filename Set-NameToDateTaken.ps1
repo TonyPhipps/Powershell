20d606382aa76a6088fec5f51fa4adecc3100a1b
Tony Phipps <anthony.james.phipps@gmail.com>
2022-09-30 15:30:08 -0600

    Update Set-NameToDateTaken.ps1

--- Set-NameToDateTaken.ps1
+++ Set-NameToDateTaken.ps1
@@ -1,26 +1,22 @@
 # Renames .jpg files to the date they were taken, according to EXIF metadata
 
 Get-ChildItem *.jpg | ForEach-Object {
-
-    $FileName = $_.FullName
-
-	$null = [reflection.assembly]::LoadWithPartialName("System.Drawing")
     
-    $pic = New-Object System.Drawing.Bitmap($FileName)
-
+    $pic = New-Object System.Drawing.Bitmap($_.FullName)
     try {
         $DateTakenRaw = $pic.GetPropertyItem(36867).Value
-        $DateTakenString = [System.Text.Encoding]::ASCII.GetString($DateTakenRaw)
-        $DateTakenParsed = [datetime]::ParseExact($DateTakenString, "yyyy:MM:dd HH:mm:ss`0", $Null)
-        Write-Verbose $DateTakenParsed
     }
-    catch {}
-    finally {
-        $pic.Dispose()
-        if ($DateTakenRaw) {
+    catch{Write-Verbose("{0} no saved date taken time" -f $_.FullName)}
+    finally{}
 
-            $NewName = ("{0:yyyy-MM-dd_HH-mm-ss}_{1}{2}" -f $DateTakenParsed, $_.Length, $_.Extension)
-            rename-item $FileName $NewName.ToLower() -Force
-        }
+    $DateTakenString = [System.Text.Encoding]::ASCII.GetString($DateTakenRaw)
+    $DateTakenParsed = [datetime]::ParseExact($DateTakenString, "yyyy:MM:dd HH:mm:ss`0", $Null)
+    $DateTakenParsed
+
+    $pic.Dispose()
+
+    if ($DateTakenParsed) {
+        $NewName = ("{0:yyyy-MM-dd_HH-mm-ss}_{1}{2}" -f $DateTakenParsed, $_.Length, $_.Extension)
+        rename-item $_.FullName $NewName.ToLower() -Force
     }
 }
