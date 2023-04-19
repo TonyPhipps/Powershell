a23e0b27394232a4c61b27d44b4556e917a3fd59
Tony Phipps <anthony.james.phipps@gmail.com>
2023-04-19 15:27:16 -0600

    Update Enable-WinRM.ps1

--- Enable-WinRM.ps1
+++ Enable-WinRM.ps1
@@ -3,10 +3,6 @@ winrm quickconfig -y
 
 # Enable WinRM Option 2
 Enable-PSRemoting –Force
-# Note: if you get an error like 
-#     "WinRM firewall exception will not work since one of	the network connection types on this machine is set to Public. Change the network connection type to either Domain or Private and try again."
-# Then you can run this
-#     Set-NetConnectionProfile -InterfaceIndex ((Get-NetConnectionProfile).InterfaceIndex) -NetworkCategory Private
 
 # Enable WinRM Option 3
 ## Start the WinRM service. 
@@ -19,3 +15,12 @@ Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies
 # Enable WinRM Option 4
 ## Run winrm quickconfig on a remote system
 PsExec.exe -s \\SomeComputer -accepteula powershell -ExecutionPolicy ByPass -nologo -command "& winrm quickconfig -y"
+
+# Dealing with the error
+#     "WinRM firewall exception will not work since one of	the network connection types on this machine is set to Public. Change the network connection type to either Domain or Private and try again."
+Set-NetConnectionProfile -InterfaceIndex ((Get-NetConnectionProfile).InterfaceIndex) -NetworkCategory Private
+
+# Dealing with the error
+#     "The following error with errorcode 0x8009030e occurred while using Kerberos authentication: A specified logon session does not exist. It may already have been terminated."
+set-item WSMan:\localhost\Client\TrustedHosts "*.contoso.com"
+set-item WSMan:\localhost\Client\TrustedHosts "192.168.1.3"
