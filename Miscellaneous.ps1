c2eaac035a72487e94f7e6f387869a7faa93a1e2
Tony Phipps <anthony.james.phipps@gmail.com>
2023-04-19 15:15:46 -0600

    Update Miscellaneous.ps1

--- Miscellaneous.ps1
+++ Miscellaneous.ps1
@@ -35,25 +35,6 @@ New-Partition -DiskNumber 1 -UseMaximumSize -AssignDriveLetter
 Format-Volume -DriveLetter F -FileSystem NTFS -NewFileSystemLabel "Label"
 
 
-# Enable WinRM Option 1
-winrm quickconfig -y
-
-# Enable WinRM Option 2
-Enable-PSRemoting –Force
-
-# Enable WinRM Option 3
-## Start the WinRM service. 
-(get-service winrm).start()
-## Set the WinRM service type to delayed auto start.
-set-service winrm -startuptype "Automatic"
-## Configure LocalAccountTokenFilterPolicy to grant administrative rights remotely to local users.
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name LocalAccountTokenFilterPolicy -Value 1
-
-# Enable WinRM Option 4
-## Run winrm quickconfig on a remote system
-PsExec.exe -s \\SomeComputer -accepteula powershell -ExecutionPolicy ByPass -nologo -command "& winrm quickconfig -y"
-
-
 # Convert .json file to PowerShell objects
 $file = "file.json"
 $json = Get-Content $file | ConvertFrom-Json
