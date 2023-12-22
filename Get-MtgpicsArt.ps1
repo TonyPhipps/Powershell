6183c4069285d2ff43f2a29b89eea82b970ff8e8
Tony Phipps <anthony.james.phipps@gmail.com>
2023-12-22 10:31:41 -0700

    Add rename sets to official abbrevation

--- Get-MtgpicsArt.ps1
+++ Get-MtgpicsArt.ps1
@@ -31,9 +31,41 @@ for ($i = -2 ; $i -le 500 ; $i++){
         $set = $regexMatch.Groups['set'].Value
         $card = $regexMatch.Groups['card'].Value
         $name = $regexMatch.Groups['name'].Value
+
+        # MTGPics.com uses unofficial set names often. Handle that here.
+        switch ($set){
+            "zve" {$setProper = "ddp"}
+            "2pc" {$setProper = "pc2"}
+            "2pd" {$setProper = "pc2"}
+            "5th" {$setProper = "5ed"}
+            "6th" {$setProper = "6ed"}
+            "7th" {$setProper = "7ed"}
+            "8th" {$setProper = "8ed"}
+            "9th" {$setProper = "9ed"}
+            "10m" {$setProper = "m10"}
+            "11m" {$setProper = "m11"}
+            "12m" {$setProper = "m12"}
+            "13m" {$setProper = "m13"}
+            "14m" {$setProper = "m14"}
+            "15m" {$setProper = "m15"}
+            "13c" {$setProper = "c13"}
+            "14c" {$setProper = "c14"}
+            "15c" {$setProper = "c15"}
+            "16c" {$setProper = "c16"}
+            "17c" {$setProper = "c17"}
+            "aki" {$setProper = "akh"}
+            "25m" {$setProper = "a25"}
+            "a22" {$setProper = "ymid"}
+            "alr" {$setProper = "arb"}
+            "alp" {$setProper = "lea"}
+            "ant" {$setProper = "atq"}
+            "apo" {$setProper = "apc"}
+            "ara" {$setProper = "arn"}
+            "tbw" {$setProper = "bro"}
+        }
         
         $percentage=[math]::Round(($count/$regexMatches.Count)*100)
-        Write-Progress -Activity "Checking for Art in Set $i ($set)" -Status "($percentage % Complete)" -PercentComplete $percentage
+        Write-Progress -Activity "Checking for Art in Set $i ($setProper)" -Status "($percentage % Complete)" -PercentComplete $percentage
         
         # Handle special characters
         $name = $name -replace "&#39;", "'"
@@ -45,14 +77,14 @@ for ($i = -2 ; $i -le 500 ; $i++){
         $filename = "{0} {1}.jpg" -f $card, $name
 
         # Output the value of the named group
-        $outFile = ("{0}\{1}\{2}" -f $output, $set, $filename)
+        $outFile = ("{0}\{1}\{2}" -f $output, $setProper, $filename)
 
         # Check if directory exists, then create.
-        if (Test-Path -Path ("{0}\{1}" -f $output, $set) -PathType Container) {
+        if (Test-Path -Path ("{0}\{1}" -f $output, $setProper) -PathType Container) {
             
         } else {
-            Write-Host ("Making dir {0}\{1}" -f $output, $set)
-            New-Item -ItemType Directory -Path ("{0}\{1}" -f $output, $set) -Force
+            Write-Host ("Making dir {0}\{1}" -f $output, $setProper)
+            New-Item -ItemType Directory -Path ("{0}\{1}" -f $output, $setProper) -Force
         }
         
         # Check if file exists, then download.
@@ -62,12 +94,12 @@ for ($i = -2 ; $i -le 500 ; $i++){
                 Invoke-WebRequest $url -OutFile $outFile
                 ++$downloaded
 
-                Write-Host ("Downloading: {0} - {1} - {2} `n`t from {3}" -f $set, $card, $name, $url)
+                Write-Host ("Downloading: {0} - {1} - {2} `n`t from {3}" -f $setProper, $card, $name, $url)
             }
             catch { Write-Verbose "$set - $card not found." }
         }
         else{
-            Write-Verbose ("Already downloaded: {0} - {1} - {2}" -f $set, $card, $name)
+            Write-Verbose ("Already downloaded: {0} - {1} - {2}" -f $setProper, $card, $name)
         }
     }
 }
