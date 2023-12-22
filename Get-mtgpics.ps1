31c59dd0cfc8082beaa7c3d424cf23d2de7625fa
Tony Phipps <anthony.james.phipps@gmail.com>
2023-12-22 08:38:09 -0700

    mostly code cleanup

--- Get-mtgpics.ps1
+++ Get-mtgpics.ps1
@@ -1,64 +1,72 @@
 ﻿# Specify where to save files
 $output = "E:\GoogleDrive\Tony\Projects\Magic\art"
 
+# -----------------------------------------------
+
 mkdir ("{0}" -f $output) -ErrorAction SilentlyContinue
 
 $downloaded = 0
 
+# For each potential set, iterate and provide progress
 for ($i = -2 ; $i -le 1000 ; $i++){
+    $percentage=[math]::Round(($i/1000)*100)
+    Write-Progress -Activity "Set $i, with $downloaded cards downloaded so far." -Status "($percentage % Complete)" -PercentComplete $percentage
     
     # Specify the URL of the web page you want to request
     $url = "https://www.mtgpics.com/art?set=$i"
 
-    # Use Invoke-WebRequest to get the web page content
+    # Get set page
     $response = Invoke-WebRequest -Uri $url -ErrorAction SilentlyContinue
-
-    # Parse the HTML content using regular expressions to find <img> tags
     $html = $response.Content
 
-    # Define a regular expression to match <img> tags and extract the 'src' attribute
-    $imgRegex = '(?s)url\(pics\/[^\/]+\/(?<set>[^\/]+)\/(?<card>\d+).jpg.*?class=und.*?\>(?<alt>[^\<]+)\<'
+    # Pull regex matches of card details and art link
+    $imgRegex = '(?s)url\(pics\/[^\/]+\/(?<set>[^\/]+)\/(?<card>\d+).jpg.*?class=und.*?\>(?<name>[^\<]+)\<'
+    #$regexMatches = ($html | Select-String -Pattern $imgRegex -AllMatches).Matches.Value
+    $regexMatches = ($html | Select-String -Pattern $imgRegex -AllMatches).Matches
 
-        # Use Select-String to find matches in the HTML content
-    $regexMatches = ($html | Select-String -Pattern $imgRegex -AllMatches).Matches.Value
+    # Iterate through each match and output the 'src' attribute
+    $count = 0
+    foreach ($regexMatch in $regexMatches) {
+        $percentage=[math]::Round(($count/$regexMatches.Count)*100)
+        Write-Progress -Activity "Downloading Art from Set" -Status "($percentage % Complete)" -PercentComplete $percentage
+        ++$count
 
-        # Iterate through each match and output the 'src' attribute
-    foreach ($value in $regexMatches) {
-        
         # Define a regular expression with a named group
-        $regex = [regex]::new('(?s)url\(pics\/[^\/]+\/(?<set>[^\/]+)\/(?<card>\d+).jpg.*?class=und.*?\>(?<alt>[^\<]+)\<')
+        #$regex = [regex]::new('(?s)url\(pics\/[^\/]+\/(?<set>[^\/]+)\/(?<card>\d+).jpg.*?class=und.*?\>(?<name>[^\<]+)\<')
+        #$set = ($regex.Match($value)).Groups['set'].Value
+        #$card = ($regex.Match($value)).Groups['card'].Value
+        #$name = ($regex.Match($value)).Groups['name'].Value
 
-        # Extract matches to a value
-        $set = ($regex.Match($value)).Groups['set'].Value
-        $card = ($regex.Match($value)).Groups['card'].Value
-        $name = ($regex.Match($value)).Groups['alt'].Value
+        $set = $regexMatch.Groups['set'].Value
+        $card = $regexMatch.Groups['card'].Value
+        $name = $regexMatch.Groups['name'].Value
            
         # Handle special characters
         $name = $name -replace "&#39;", "'"
         $name = $name -replace ":", ""
+        $name = $name -replace "!", ""
 
         $url = "https://www.mtgpics.com/pics/art/" + $set + "/" + $card + ".jpg"
         $filename = "{0} {1}.jpg" -f $card, $name
 
         # Output the value of the named group
-        
         $outFile = ("{0}\{1}\{2}" -f $output, $set, $filename)
 
-        
-        # Test if the directory exists
+        # Check if directory exists, then create.
         if (Test-Path -Path ("{0}\{1}" -f $output, $set) -PathType Container) {
             
         } else {
             mkdir ("{0}\{1}" -f $output, $set)
         }
         
+        # Check if file exists, then download.
         if (-not(Test-Path -Path $outFile -PathType Leaf)) {
 
             try { 
                 Invoke-WebRequest $url -OutFile $outFile
                 ++$downloaded
 
-                write-host ("Pulling`n`t{0}`n`t`tFrom`n`t`t`t{1}" -f $filename, $url)
+                Write-host ("Downloading: {0} - {1} - {2} `n`t from {3}" -f $set, $card, $name, $url)
             }
             catch { Write-Verbose "$set - $card not found." }
         }
