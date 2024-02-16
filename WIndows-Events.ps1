2062b2d0422ce7cdf35cc7cd6628be14ef04d828
Tony Phipps <anthony.james.phipps@gmail.com>
2024-02-16 09:08:29 -0700

    Update WIndows-Events.ps1

--- WIndows-Events.ps1
+++ WIndows-Events.ps1
@@ -28,3 +28,26 @@ Get AppLocker Logs (different info)
 Get-AppLockerFileInformation –EventLog -LogPath "Microsoft-Windows-AppLocker/EXE and DLL" –EventType Audited –Statistics
 ```
 
+Enable Windows Event Logs
+```
+$LogNames = @(
+    "Microsoft-Windows-DriverFrameworks-UserMode/Operational",
+    "Microsoft-Windows-Dhcp-Server/AuditLog",
+    "Microsoft-Windows-Dhcp-Server/DebugLogs",
+    "Microsoft-Windows-Dhcp-Server/FilterNotifications",
+    "Microsoft-Windows-Dhcp-Server/Operational",
+    "Microsoft-Windows-Dhcpv6-Client/Admin",
+    "Microsoft-Windows-Dhcpv6-Client/Operational"
+  )
+ForEach ($LogName in $LogNames) {
+    $EventLog = Get-WinEvent -ListLog $LogName
+    if ($EventLog.IsEnabled) {
+        Write-Host "Already enabled: $LogName"
+    }
+    else {
+        Write-Host "Enabling: $LogName"
+        $EventLog.IsEnabled = $true
+        $EventLog.SaveChanges()
+    }
+}
+```
