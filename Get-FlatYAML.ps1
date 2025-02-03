8c5500fc5d82f4b9326f8e7e2200d1e3cd081712
Tony Phipps <anthony.james.phipps@gmail.com>
2025-02-03 12:49:24 -0700

    Fixes

--- Get-FlatYAML.ps1
+++ Get-FlatYAML.ps1
@@ -15,7 +15,7 @@
     if ($inputObject -is [Hashtable]){
         foreach ($Key in $InputObject.Keys) { # review each member
             $Member = $InputObject.$Key
-            if ($member -is [Hashtable]){
+            if ($Member -is [Hashtable]){
                 #-----------------Level 2-----------------
                 foreach ($Key2 in $Member.Keys) { # review each member
                     $Member2 = $Member.$Key2
@@ -26,51 +26,46 @@
                             if ($Member3 -is [Hashtable]){
                                 Write-Host "is hashtable: $Key.$Key2.$Key3. Add another level."
                             }
-                            elseif ($Member2.$Key3 -is [System.Collections.ICollection]) {
-                                #Write-Host "is ICollection: $Key.$Key2.$Key3"
+                            elseif ($Member3 -is [System.Collections.ICollection]) {
                                 $ICollection3 = $Member2.$Key3 -join ", "
                                 $Output | Add-Member -MemberType NoteProperty -Name ($Key + "." + $Key2 + "." + $Key3) -Value $ICollection3 -ErrorAction SilentlyContinue | Out-Null
                             }
-                            elseif ($Member3.GetType().Name -is [String]) {
-                                #Write-Host "is string: $Key.$Key2.$Key3"
+                            elseif ($NULL -eq $Member3) {
+                            }
+                            elseif ($Member3.GetType().Name -in ("String","Int32","long","Bool")) {
                                 $Output | Add-Member -MemberType NoteProperty -Name ($Key + "." + $Key2 + "." + $Key3) -Value $Member3 -ErrorAction SilentlyContinue | Out-Null
                             }
                             else {
-                                Write-Host "is ($Member3.GetType().Name): $Key.$Key2.$Key3" 
+                                Write-Host "Level 3 - $Key.$Key2.$Key3 is a $($Member3.GetType())" 
                             }            
                         }
                         #-----------------Level 3 END-----------------
                     }
                     elseif ($Member.$Key2 -is [System.Collections.ICollection]) {
-                        #Write-Host "is ICollection: $Key.$Key2"
                         $ICollection2 = $Member2.$Key2 -join ", "
                         $Output | Add-Member -MemberType NoteProperty -Name ($Key + "." + $Key2) -Value $ICollection2 -ErrorAction SilentlyContinue | Out-Null
                     }
-                    elseif ($Member2.GetType().Name -is [String]) {
-                        #Write-Host "is string: $Key.$Key2"
+                    elseif ($Member2.GetType().Name -in ("String","Int32","long","Bool")) {
                         $Output | Add-Member -MemberType NoteProperty -Name ($Key + "." + $Key2) -Value $Member2 -ErrorAction SilentlyContinue | Out-Null
                     }
                     else {
-                        #Write-Host "is ($Member2.GetType().Name): $Key.$Key2" 
+                        Write-Host "Level 2 - $Key.$Key2 is a $($Member2.GetType())" 
                     }               
                 #-----------------Level 2 END-----------------
                 }
             }
             elseif ($InputObject.$Key -is [System.Collections.ICollection]) {
-                #Write-Host "is ICollection: $Key"
                 $ICollection = $InputObject.$key -join ", "
                 $Output | Add-Member -MemberType NoteProperty -Name $Key -Value $ICollection -ErrorAction SilentlyContinue | Out-Null
             }
-            elseif ($Member.GetType().Name -is [String]) {
-                #Write-Host "is string: $Key"
+            elseif ($Member.GetType().Name -in ("String","Int32","long","Bool")) {
                 $Output | Add-Member -MemberType NoteProperty -Name $Key -Value $Member -ErrorAction SilentlyContinue | Out-Null
             }            
             else {
-                Write-Host "is ($Member.GetType().Name): $Key" 
+                $Member3.GetType()
+                Write-Host "Level 1 - $Key is a $($Member.GetType())" 
             }               
         }
     }
     return $Output
 }
-
-Flatten-Object($YamlObjects[5])
