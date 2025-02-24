9b1316136f5a55809b2d8a99770cccd5877cfdcd
Tony Phipps <anthony.james.phipps@gmail.com>
2025-02-24 15:34:26 -0700

    update

--- Sigma-Convert.ps1
+++ Sigma-Convert.ps1
@@ -1,10 +1,4 @@
-﻿# To demonstrate bulk conversion using powershell to loop through each rule, creating an independent output file per rule
-
-# cd $HOME
-# python -m venv sigma
-
-Set-Location $HOME\python\sigma
-$venv = "C:\Users\username\python\sigma"
+﻿$venv = "C:\Users\youruser\python\sigma"
 $inputDir = "./sigma-master/rules/windows/"
 $outputDir = "./output"
 
@@ -19,8 +13,9 @@ Get-ChildItem -Path $inputDir -Recurse -Filter "*.yml" | ForEach-Object {
     $outputFile = "$outputDir/$ruleName.txt"
     
     Write-Host "Converting: $($_.FullName) -> $outputFile"
-    
+
     sigma convert --target splunk --pipeline splunk_windows --output $outputFile $_.FullName
-    # -f savedsearches
+    
 }
+
 deactivate
