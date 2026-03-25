72d63bb472d5d7abda682d256d73823f62a6d3a7
Tony Phipps <anthony.james.phipps@gmail.com>
2026-03-25 15:44:17 -0600

    added rsat stuff

--- TonysWIn11.ps1
+++ TonysWIn11.ps1
@@ -22,7 +22,19 @@ function Set-RegProperty($FullPath, $PropertyType, $Value){
     
 }
 
+
 # Disable "Show more options" context menu in Windows 11
 Set-RegProperty "HKCU:\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" "DWORD" 1
 reg add "HKCU:\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve​
 
+
+# Install RSAT: Active Directory Tools
+Get-WindowsCapability -Online -Name "Rsat.ActiveDirectory.DS-LDS.Tools*" | Add-WindowsCapability -Online
+
+
+# Install GPO Tools:
+Get-WindowsCapability -Online -Name "Rsat.GroupPolicy.Management.Tools*" | Add-WindowsCapability -Online
+
+
+# Install DNS Server Tools:
+Get-WindowsCapability -Online -Name "Rsat.Dns.Tools*" | Add-WindowsCapability -Online
