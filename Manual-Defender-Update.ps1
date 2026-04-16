58152f26552c77e17830bb9b6a6a624b0ac31eb6
Tony Phipps <anthony.james.phipps@gmail.com>
2026-04-16 14:54:23 -0600

    deleteme

--- Manual-Defender-Update.ps1
+++ Manual-Defender-Update.ps1
@@ -3,7 +3,7 @@ Get-MpPreference | Select-Object SignatureDefinitionUpdateFileSharesSources, Sig
 Get-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender" -Name "DisableAntiSpyware"
 Get-ChildItem "C:\ProgramData\Microsoft\Windows Defender\Platform\"
 Start-Process -FilePath "\\yourhost\DefenderUpdates\updateplatform.amd64fre_ef488e855b50b875137a5d335e4a6a76c186992a.exe" -ArgumentList "/quiet /norestart" -Wait
-$sigFile = "\\ICS-SV22-Update\DefenderUpdates\x64\mpam-fe.exe"
+$sigFile = "\\yourhost\DefenderUpdates\x64\mpam-fe.exe"
 (Get-Item $sigFile).VersionInfo | Select-Object FileVersion, ProductVersion
 Update-MpSignature -UpdateSource FileShares
 Start-Process "\\yourhost\DefenderUpdates\x64\mpam-fe.exe" -ArgumentList "-mpsigstub" -Wait
