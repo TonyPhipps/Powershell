3df32bc1183b689e0d716e5b156e85c0524960f1
Tony Phipps <anthony.james.phipps@gmail.com>
2026-05-27 22:35:32 -0600

    new file

---
+++ Find-MagicArtFile.ps1
@@ -0,0 +1,13 @@
+$filter = "CardName"
+$path = "F:\GoogleDrive\Tony\Projects\Magic\art"
+
+$shell = New-Object -ComObject Shell.Application
+Get-ChildItem -Path $path -Filter "*$filter*" -Recurse -File | ForEach-Object {
+    $folder = $shell.Namespace($_.DirectoryName)
+    $fileItem = $folder.ParseName($_.Name)
+    $dimensions = $folder.GetDetailsOf($fileItem, 31) # Detail rule 31 is standard for "Dimensions" in modern Windows environments
+    [PSCustomObject]@{
+        "Full Path"  = $_.FullName
+        "Dimensions" = if ($dimensions) { $dimensions } else { "N/A (Not an image or property missing)" }
+    }
+} | Format-Table -AutoSize
